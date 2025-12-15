d8793eee05f8025c203793f1c00990d47c206055
alvaro-gargon <alvaro.garcia.daw@gmail.com>
2025-12-15 12:54:55 +0100

    feat - now flow works

---
+++ config/confAPP.php
@@ -0,0 +1,17 @@
+<?php
+
+/*  Nombre: Alvaro Garcia Gonzalez
+*   Fecha: 15/12/2025
+*   Uso:  requires de todos los archivos del modelo necesitado*/ 
+    
+require_once 'core/231018libreriaValidacion.php';
+
+$controller=[
+    'inicioPublico' => 'controller/cInicioPublico.php'
+];
+
+$view=[
+    'layout' => 'view/Layout.php',
+    'inicioPublico' => 'view/vInicioPublico.php'
+]
+?>
---
+++ indexLoginLogoff.php
@@ -0,0 +1,16 @@
+<?php
+        /* Nombre: Alvaro Garcia Gonzalez
+        * Fecha: 15/12/2025
+        * Uso:  index de la aplicacion*/
+        
+        //incluyo la configuracion 
+        require_once 'config/confAPP.php';
+        require_once 'config/confDBPDO.php';
+        //inicio la sesion
+        session_start();
+        
+        if(!isset($_SESSION['paginaEnCurso'])){
+            $_SESSION['paginaEnCurso']='inicioPublico';
+        }
+        require_once $controller[$_SESSION['paginaEnCurso']];
+    ?>
