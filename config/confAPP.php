6a5dea9e07eedfd8456434aac953bc19dae7b62e
alvaro-gargon <alvaro.garcia.daw@gmail.com>
2025-12-15 13:08:52 +0100

    add - comments in files

--- config/confAPP.php
+++ config/confAPP.php
@@ -3,13 +3,18 @@
 /*  Nombre: Alvaro Garcia Gonzalez
 *   Fecha: 15/12/2025
 *   Uso:  requires de todos los archivos del modelo necesitado*/ 
-    
+//incluyo la libreria de validacion
 require_once 'core/231018libreriaValidacion.php';
 
+//aqui se incluyen todos los archivos del modelo
+
+
+//array para cargar los archivos del controlador
 $controller=[
     'inicioPublico' => 'controller/cInicioPublico.php'
 ];
 
+//array para cargar los archivos de la vista
 $view=[
     'layout' => 'view/Layout.php',
     'inicioPublico' => 'view/vInicioPublico.php'
